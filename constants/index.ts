dbbfc906f1db70651cbe52f23801e6434a40b135
edsonalcala <edson_alcala@msn.com>
2024-04-29 12:01:47 -0600

    change github url

--- constants/index.ts
+++ constants/index.ts
@@ -3,5 +3,5 @@ export const PROJECT_CONFIG = {
     footer: 'EarthMind',
     discord: 'https://discord.com',
     twitter: 'https://twitter.com',
-    repository: 'https://github.com/shuding/nextra-docs-template'
+    repository: 'https://github.com/earth-mind/docs'
 }
