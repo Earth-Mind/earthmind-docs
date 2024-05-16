d6665c24b928f7d8eec3427d7d7f646260b1d06e
edsonalcala <edson_alcala@msn.com>
2024-05-16 11:54:23 -0500

    added logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,8 +3,22 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 import { PROJECT_CONFIG } from "./constants";
 
+const logoStyle = {
+    display: "flex",
+    alignItems: "center",
+    fontSize: "1.45rem",
+    fontWeight: "bold",
+};
+
 const config: DocsThemeConfig = {
-    logo: <span>{PROJECT_CONFIG.name}</span>,
+    logo: (
+        <div style={logoStyle}>
+            <img height="25" width="25" src="/images/logo.png" />
+            <span className="custom-header" style={{ marginLeft: "8px" }}>
+                {PROJECT_CONFIG.name} Docs
+            </span>
+        </div>
+    ),
     project: {
         link: PROJECT_CONFIG.repository,
     },
