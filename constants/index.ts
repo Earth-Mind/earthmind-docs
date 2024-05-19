3a99f0b2873df22103f23091691808f5d243a6f0
edsonalcala <edson_alcala@msn.com>
2024-05-19 12:49:26 -0600

    roadmap finished

--- constants/index.ts
+++ constants/index.ts
@@ -4,5 +4,6 @@ export const PROJECT_CONFIG = {
     discord: 'https://discord.com',
     twitter: 'https://twitter.com',
     repository: 'https://github.com/earth-mind/docs',
-    description: 'Documentation of the EarthMind project'
+    description: 'Documentation of the EarthMind project',
+    website: 'https://earthmind.tech',
 }
